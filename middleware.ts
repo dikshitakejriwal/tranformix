9a51decb8246103aac5081bba62f7297f08a2d3e
Dikshita Kejriwal <djk5958@psu.edu>
2024-03-04 08:36:49 -0500

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
+// allowing public access to this api endpoint so clerk can ping it with new events
 export default authMiddleware({
- 
+  publicRoutes: ['/api/webhooks/clerk']
 });
  
 export const config = {
